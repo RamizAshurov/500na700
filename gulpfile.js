484d91c3523ff73445e447af0438318e53177a31
Ramiz Ashurov <dima-sedov-1996@mail.ru>
2022-04-06 14:03:24 +0300

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,12 @@ const gulp = require("gulp");
 const dartSass = require("sass")
 const sass = require("gulp-sass")(dartSass);
 const browserSync = require("browser-sync");
-const del = require("del")
-
-function copy () {
-    return gulp.src("./*.txt")
-        .pipe(gulp.dest("./dest/files"))
-}
 
 function scss() {
     return gulp.src("./style.scss")
         .pipe(sass())
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream())
-}
-
-function reset() {
-    return del("./dest/files")
 }
 
 function watch() {
